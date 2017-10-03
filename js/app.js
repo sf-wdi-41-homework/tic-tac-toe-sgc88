99da461231d5b934a97eda51da050bf27bf5199a
sgc88 <sehribancheng@yahoo.com>
2017-10-02 23:18:53 -0700

    able to change of the boxes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 // wait for the DOM to finish loading
+console.log('sanity check');
+var turns=0;
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  $(".box").on("click", function(){
 
+
+  	if(turns % 2 === 0){
+
+  		$(this).text("X");
+  		turns++
+  	}else{
+  		  
+  		$(this).text("O");
+  		turns++
+  	}
+  });
 });
