4ce57006194c55962b1e71a0417e6919ac572d9e
sgc88 <sehribancheng@yahoo.com>
2017-10-02 23:46:56 -0700

    added some pseudocode

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   // goes inside this function
   $(".box").on("click", function(){
 
-
+  	// X and O should take a turn with % module (even and odd- even for X, odd for O)
   	if(turns % 2 === 0){
 
   		$(this).text("X");
