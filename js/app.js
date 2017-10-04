4a55d437b75461c40ca96ffdaf13c4d1d93a74df
sgc88 <sehribancheng@yahoo.com>
2017-10-03 23:32:03 -0700

    set up the box is already taken

--- js/app.js
+++ js/app.js
@@ -16,40 +16,42 @@ $(document).ready(function() {
 
 
   $(".box").on("click", function(){
+    if($(this).text() === ""){
+      // X and O should take a turn with % module (even and odd- even for X, odd for O)
+      if(turns % 2 === 0){
+        $(this).text("X");
+        // increments turns variable
+        turns++;
+      }else{
+        $(this).text("O");
+        turns++;
+      }
 
-  	// X and O should take a turn with % module (even and odd- even for X, odd for O)
-  	if(turns % 2 === 0){
-
-  		$(this).text("X");
-      // increments turns variable
-  		turns++;
-  	}else{
-
-  		$(this).text("O");
-  		turns++;
-  	}
-
-    if( $(".1").text() === "O" && $(".2").text() === "O" && $(".3").text() === "O"
-        || $(".1").text() === "X" && $(".2").text() === "X" && $(".3").text() === "X"
-        || $(".4").text() === "O" && $(".5").text() === "O" && $(".6").text() === "O"
-        || $(".4").text() === "X" && $(".5").text() === "X" && $(".6").text() === "X"
-        || $(".7").text() === "O" && $(".8").text() === "O" && $(".9").text() === "O"
-        || $(".7").text() === "X" && $(".8").text() === "X" && $(".9").text() === "X"
-        || $(".1").text() === "O" && $(".4").text() === "O" && $(".7").text() === "O"
-        || $(".1").text() === "X" && $(".4").text() === "X" && $(".7").text() === "X"
-        || $(".2").text() === "O" && $(".5").text() === "O" && $(".8").text() === "O"
-        || $(".2").text() === "X" && $(".5").text() === "X" && $(".8").text() === "X"
-        || $(".3").text() === "O" && $(".6").text() === "O" && $(".9").text() === "O"
-        || $(".3").text() === "X" && $(".6").text() === "X" && $(".9").text() === "X"
-        || $(".1").text() === "O" && $(".5").text() === "O" && $(".9").text() === "O"
-        || $(".1").text() === "X" && $(".5").text() === "X" && $(".9").text() === "X"
-        || $(".3").text() === "O" && $(".5").text() === "O" && $(".7").text() === "O"
-        || $(".3").text() === "X" && $(".5").text() === "X" && $(".7").text() === "X"
-    ){
-      console.log("You win");
-    } else if ($(".1").text() !== "" && $(".2").text() !== "" && $(".3").text() !== "" && $(".4").text() !== "" && $(".5").text() !== "" && $(".6").text() !== "" && $(".7").text() !== "" && $(".8").text() !== "" && $(".9").text() !== ""){
-      console.log("no one wins");
+      if( $(".1").text() === "O" && $(".2").text() === "O" && $(".3").text() === "O"
+          || $(".1").text() === "X" && $(".2").text() === "X" && $(".3").text() === "X"
+          || $(".4").text() === "O" && $(".5").text() === "O" && $(".6").text() === "O"
+          || $(".4").text() === "X" && $(".5").text() === "X" && $(".6").text() === "X"
+          || $(".7").text() === "O" && $(".8").text() === "O" && $(".9").text() === "O"
+          || $(".7").text() === "X" && $(".8").text() === "X" && $(".9").text() === "X"
+          || $(".1").text() === "O" && $(".4").text() === "O" && $(".7").text() === "O"
+          || $(".1").text() === "X" && $(".4").text() === "X" && $(".7").text() === "X"
+          || $(".2").text() === "O" && $(".5").text() === "O" && $(".8").text() === "O"
+          || $(".2").text() === "X" && $(".5").text() === "X" && $(".8").text() === "X"
+          || $(".3").text() === "O" && $(".6").text() === "O" && $(".9").text() === "O"
+          || $(".3").text() === "X" && $(".6").text() === "X" && $(".9").text() === "X"
+          || $(".1").text() === "O" && $(".5").text() === "O" && $(".9").text() === "O"
+          || $(".1").text() === "X" && $(".5").text() === "X" && $(".9").text() === "X"
+          || $(".3").text() === "O" && $(".5").text() === "O" && $(".7").text() === "O"
+          || $(".3").text() === "X" && $(".5").text() === "X" && $(".7").text() === "X"
+      ){
+        console.log("You win");
+      } else if ($(".1").text() !== "" && $(".2").text() !== "" && $(".3").text() !== "" && $(".4").text() !== "" && $(".5").text() !== "" && $(".6").text() !== "" && $(".7").text() !== "" && $(".8").text() !== "" && $(".9").text() !== ""){
+        console.log("no one wins");
+      }
+    }else{
+      // nothing happens box is taken
     }
+
 
   });
 });
